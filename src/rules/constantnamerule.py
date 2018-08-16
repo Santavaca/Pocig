ea0ea1b80c30b2a6238a5741160e057adbb4c25c
vincentolsen <31868348+vincentolsen@users.noreply.github.com>
2018-08-16 09:43:04 -0700

    Update constantnamerule.py
    fix typo

--- src/rules/constantnamerule.py
+++ src/rules/constantnamerule.py
@@ -1,5 +1,5 @@
 class ConstantNameRule():
-    def __init__(self, address):  # by convention, all rules will have an init method that accepts and address
+    def __init__(self, address):  # by convention, all rules will have an init method that accepts an address
         self.address = address
 
     def run(self):  # by convention all rules will have a run method that returns an owner
