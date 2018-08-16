6d526d06e4f69511f4700840396f658205ef6222
vincentolsen <31868348+vincentolsen@users.noreply.github.com>
2018-08-15 17:00:02 -0700

    white space

--- src/rules/constantnamerule.py
+++ src/rules/constantnamerule.py
@@ -1,7 +1,7 @@
 class ConstantNameRule():
-    def __init__(self, address): # by convention, all rules will have an init method that accepts and address
+    def __init__(self, address):  # by convention, all rules will have an init method that accepts and address
         self.address = address
 
-    def run(self): # by convention all rules will have a run method that returns an owner
-        owner = Owner(first_name = 'constant', last_name = 'name')
+    def run(self):  # by convention all rules will have a run method that returns an owner
+        owner = Owner(first_name='constant', last_name='name')
         return owner
